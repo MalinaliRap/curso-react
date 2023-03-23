0d69ca1f147ffe52c3d016806e157a37b1ca229e
MalinaliRap <utfprdiario@gmail.com>
2023-03-23 16:34:48 -0300

    Aula #8

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,21 @@
+import  PropTypes  from "prop-types";
 
-function Item({marca}) {
+function Item({marca, ano_lancamento}) {
     return (
         <>
-           <li>{marca}</li> 
+           <li>{marca} - {ano_lancamento}</li> 
         </>
     );
+}
+
+Item.propTypes = {
+    marca: PropTypes.string.isRequired,
+    ano_lancamento: PropTypes.number.isRequired
+}
+
+Item.defaultProps = {
+    marca: 'Faltou marca',
+    ano_lancamento: 0
 }
 
 export default Item;
