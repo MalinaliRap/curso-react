ec162c2dee5db42c9e697fcc20f992872015f157
MalinaliRap <utfprdiario@gmail.com>
2023-03-23 15:23:04 -0300

    Aula #6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   
 
   return (
     <div className="App">
+      <Frase />
+      <h1>Testando CSS</h1>
       <Pessoa nome="Mali" idade="28" profissao="Programador" foto="https://via.placeholder.com/150"/>
     </div>
   );
