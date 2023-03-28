0702e21a220ea8f3c7725220b2b2f689c10fef8a
MalinaliRap <utfprdiario@gmail.com>
2023-03-27 21:00:46 -0300

    Aula #13

---
+++ src/components/OutraLista.js
@@ -0,0 +1,21 @@
+function OutraLista({itens}) {
+    return (
+        <>
+            <h3>Lista de coisas boas</h3>
+            { itens.length > 0 ? (
+                
+                itens.map((item, index) => (
+                    <p key={index}>{item}</p>
+                ))
+            ) : (
+                <p>
+                    Não existe itens na lista
+                </p>
+            )
+
+            }
+        </>
+    );
+}
+
+export default OutraLista;
